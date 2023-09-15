9f996709333bf59e861de28fc4f6d9df44b07565
robertandreasian <106664436+robertandreasian@users.noreply.github.com>
2023-09-15 13:34:49 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Card from "./components/UI/Card";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: "Study",
+      amount: 300,
+      date: new Date(2021, 2, 20),
+    },
+    {
+      id: 'e2',
+      title: "Goods",
+      amount: 400,
+      date: new Date(2021, 4, 12),
+    },
+    {
+      id: 'e3',
+      title: "Petrol",
+      amount: 350,
+      date: new Date(2021, 2, 1),
+    },
+  ];
+  return (
+    <div className="app">
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
